adb38ea1544130247f99467a54e48ccd5d3c99f9
diegotosato <tosato1998@gmail.com>
2025-10-17 16:00:17 +0200

    copy exercise track and write 1st psrt of pseudo-code

--- snack-2/main.js
+++ snack-2/main.js
@@ -1,1 +1,25 @@
-console.log('worka');
+/*
+Snack2**
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+*/
+
+/*
+PSEUDO-CODE
+
+- creo un array di oggetti
+- oggetto
+    - nome
+    - punti fatti = 0
+    - falli subiti = 0
+
+- ciclo nel array
+    - ad ogni interazione
+        - array[i].punti fatti = math random
+        - array[i].falli subiti = math random
+
+- loggo primo array e vedo se cambiano i numeri
+*/
