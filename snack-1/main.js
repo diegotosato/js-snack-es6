5bc7c2c8211474cf429be7daafdc0dcf832fdce9
diegotosato <tosato1998@gmail.com>
2025-10-17 15:21:46 +0200

    create supp array to inglobe the key's object that i want with loop

--- snack-1/main.js
+++ snack-1/main.js
@@ -12,7 +12,7 @@ Pseudo-Code
     - nome
     - peso
 
--creo un array di supporto dove pusho l'oggetto con il peso minore
+-creo un array di supporto dove pusho il valore delle chiavi peso
 - ciclo for
     - scorro nell'array degli oggetti
         - i = 0, i < array oggetti.lenght, i++
@@ -36,31 +36,60 @@ Pseudo-Code
 /* ------------------------------------------------------------------------------------------------------------*/
 
 
-// - creo un array
+/* - creo un array
+- dentro creo degli oggetti
+- chiave/valore:
+    - nome
+    - peso
+    */
 const bycicles = [
     {
-        name : 'Canyon',
-        peso : 48
+        name: 'Canyon',
+        peso: 48
     },
     {
-        name : 'Pinarello',
-        peso : 33
+        name: 'Pinarello',
+        peso: 33
     },
     {
-        name : 'Van Rysel',
-        peso : 60
+        name: 'Van Rysel',
+        peso: 60
     },
     {
-        name : 'Bianchi',
-        peso : 55
+        name: 'Bianchi',
+        peso: 55
     },
     {
-        name : 'Guerciotti',
-        peso : 51
+        name: 'Guerciotti',
+        peso: 51
     },
     {
-        name : 'Cannondale',
-        peso : 42
+        name: 'Cannondale',
+        peso: 42
     }
 ]
 
+console.log(bycicles);
+
+
+
+
+// -creo un array di supporto dove pusho il valore delle chiavi peso
+const byciclesWeights = []
+
+/*
+- ciclo for
+    - scorro nell'array degli oggetti
+        - i = 0, i < array oggetti.lenght, i++
+        - const bici_weight = array oggetti [i].peso
+        */
+for (let i = 0; i < bycicles.length; i++) {
+    const bycicleWeight = bycicles[i].peso;
+    console.log(bycicleWeight);
+
+    //- pusho nel array di supporto i pesi
+    byciclesWeights.push(bycicleWeight)
+
+}
+
+console.log(byciclesWeights);
